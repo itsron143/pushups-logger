584ff1cfa10e91028174f74e6331b4e608ff780d
Rounak Vyas <itsron143@gmail.com>
2020-07-21 10:48:22 +0530

    Added video 2 and 3

---
+++ pl_video_3_templates/main.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template, url_for
+
+
+main = Blueprint('main', __name__)
+
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+
+@main.route('/profile')
+def profile():
+    return render_template('profile.html')
