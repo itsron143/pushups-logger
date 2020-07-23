db55f056d6a002db39b56e4fb8652992de29c783
Rounak Vyas <itsron143@gmail.com>
2020-07-23 16:49:26 +0530

    Added Video 4 Code

---
+++ pl_video_4_forms/auth.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, render_template, url_for, request, redirect
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    print(email, name, password)
+
+    return redirect(url_for('auth.login'))
+
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    print(email, password)
+
+    return redirect(url_for('main.profile'))
+
+
+@auth.route('/logout')
+def logout():
+    return "Use this to log out."
